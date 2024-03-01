9bbe2dcd42bf4aaefc4de625efe2e2a7a675976c
Ahsan Javed <ahsannjavaid@gmail.com>
2024-03-01 18:09:12 +0500

    UI kit quickstart add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import AgoraUIKit from "agora-react-uikit";
+
 function App() {
+  const [videoCall, setVideoCall] = useState(true);
+
+  const rtcProps = {
+    appId: "3d1f6eddcea44e1cba76d97ca4b6b4bd",
+    channel: "TestingForIOS",
+    token:
+      "007eJxTYEi7vkPi+zKRVOEDNy2u9StcS1ku/+XQktjzmk8eyP5fEPBFgcE4xTDNLDUlJTk10cQk1TA5KdHcLMXSPDnRJMksySQpJWn1w9SGQEYG49oKJkYGCATxeRlCUotLMvPS3fKLPP2DGRgAmaombQ==",
+  };
+
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  };
+
   return (
-    <div>
-      GetTogether
-    </div>
+    <>
+      <h4 style={{ textAlign: "center" }}>GetTogether (Coming Soon)</h4>
+      {videoCall ? (
+        <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+          <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+        </div>
+      ) : (
+        <h3 onClick={() => setVideoCall(true)}>Join</h3>
+      )}
+    </>
   );
 }
 
